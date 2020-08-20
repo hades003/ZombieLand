1df8603d4cb78c5b215b6a828d454692245293fa
Hades <chasewolf@outlook.com>
2020-08-20 13:35:18 -0500

    Finished body get and set methods, and deterioration method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Runtime.CompilerServices;
 using System.Transactions;
 
@@ -16,11 +16,23 @@ namespace ZombieLand
     public class body
     {
 
-        protected String name;
+        private String name;
+        private int speed = 4;
+        private int preception = 6;
 
         public body(String nm) 
         {
             name = nm;
+        }
+
+        public body() 
+        {
+            name = null;
+        }
+
+        public void setName(String set)
+        {
+            name = set;
         }
 
         private bool walking = true;
@@ -71,7 +83,7 @@ namespace ZombieLand
             return ears;
         }
 
-        public char getheart() 
+        public char getHeart() 
         {
             return heart;
         }
@@ -121,7 +133,33 @@ namespace ZombieLand
             return leftSholder;
         }
 
+        public char getLeftElbow()
+        {
+            return leftElbow;
+        }
+
+        public char getLeftForearm()
+        {
+            return leftForearm;
+        }
+
+        public char getRightSholder()
+        {
+            return rightSholder;
+        }
+
+        public char getRightElbow()
+        {
+            return rightElbow;
+        }
+
+        public char getRightForearm()
+        {
+            return rightForearm;
+        }
+
         //these are all of the set methods
+        //head section
         public void setEyes(char set) 
         {
             eyes = set;
@@ -136,27 +174,112 @@ namespace ZombieLand
         {
             ears = set;
         }
+
+        //torso section
+        public void setHeart(char set)
+        {
+            heart = set;
+        }
+
+        public void setGuts(char set)
+        {
+            guts = set;
+        }
+
+        public void setLungs(char set)
+        {
+            lungs = set;
+        }
+
+        //leg section(left and knight)
+        public void setLeftThigh(char set)
+        {
+            leftThigh = set;
+        }
+
+        public void setLeftKnee(char set)
+        {
+            leftKnee = set;
+        }
+
+        public void setLeftShin(char set)
+        {
+            leftShin = set;
+        }
+
+        public void setRightThigh(char set)
+        {
+            rightThigh = set;
+        }
+
+        public void setRightKnee(char set)
+        {
+            rightKnee = set;
+        }
+
+        public void setRightShin(char set)
+        {
+            rightShin = set;
+        }
+
+        //arms sections, both right and left
+        public void setRightSholder(char set)
+        {
+            rightSholder = set;
+        }
+
+        public void setRightElbow(char set)
+        {
+            rightElbow = set;
+        }
+
+        public void setRightForearm(char set)
+        {
+            rightForearm = set;
+        }
+
+        public void setLeftSholder(char set)
+        {
+            leftSholder = set;
+        }
+
+        public void setLeftElbow(char set)
+        {
+            leftElbow = set;
+        }
+
+        public void setLeftForearm(char set)
+        {
+            leftForearm = set;
+        }
+
+        //This is the check damage method of which sees if any body parts need to fall off,
+        // if the zombie should be dead or not, and if the speed of the zombie should be changed
+        public void checkDamage() 
+        {
+
+        }
     }
 
 
     //this is the zombie body stats that are used for enemies. it deteriates upon generation.
     public class zombieBody : body
     {
-        private static int deteriation;
-
         public zombieBody(String nm, int det)
         {
-            deteriation = det;
-            name = nm;
+            setName(nm);
+            deter(det);
         }
-
-        private static Random qwe = new Random();
-        private int trueDet = qwe.Next(deteriation);
         
-        //this is the method that generates the zombie deteriation
-        public void deter()
+        //this is the method that generates the zombie deteriation. 
+        // 'f' means fine and has taken no damage, 'd' means that it's taken considerable damage,
+        //and 'g' means that the body part has completely been removed.
+        public void deter(int dtr)
         {
-            int targets = 0;
+            Random qwe = new Random();
+            int trueDet = qwe.Next(dtr);
+
+            int targets;
             bool control;
             for (int i = 0; i < trueDet; i++)
             {
@@ -208,8 +331,309 @@ namespace ZombieLand
                             control = false;
                         }
                     }
+                    else if (targets == 4)
+                    {
+                        if (getHeart() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setHeart('d');
+                            }
+                            else
+                            {
+                                setHeart('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 5)
+                    {
+                        if (getLungs() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setLungs('d');
+                            }
+                            else
+                            {
+                                setLungs('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 6)
+                    {
+                        if (getGuts() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setGuts('d');
+                            }
+                            else
+                            {
+                                setGuts('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 7)
+                    {
+                        if (getLeftSholder() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setLeftSholder('d');
+                            }
+                            else
+                            {
+                                setLeftSholder('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 8)
+                    {
+                        if (getLeftElbow() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setLeftElbow('d');
+                            }
+                            else
+                            {
+                                setLeftElbow('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 9)
+                    {
+                        if (getLeftForearm() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setLeftForearm('d');
+                            }
+                            else
+                            {
+                                setLeftForearm('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 10)
+                    {
+                        if (getRightSholder() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setRightSholder('d');
+                            }
+                            else
+                            {
+                                setRightSholder('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 11)
+                    {
+                        if (getRightElbow() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setRightElbow('d');
+                            }
+                            else
+                            {
+                                setRightElbow('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 12)
+                    {
+                        if (getRightForearm() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setRightForearm('d');
+                            }
+                            else
+                            {
+                                setRightForearm('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 13)
+                    {
+                        if (getLeftThigh() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setLeftThigh('d');
+                            }
+                            else
+                            {
+                                setLeftThigh('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 14)
+                    {
+                        if (getLeftKnee() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setLeftKnee('d');
+                            }
+                            else
+                            {
+                                setLeftKnee('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 15)
+                    {
+                        if (getLeftShin() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setLeftShin('d');
+                            }
+                            else
+                            {
+                                setLeftShin('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 16)
+                    {
+                        if (getRightThigh() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setRightThigh('d');
+                            }
+                            else
+                            {
+                                setRightThigh('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else if (targets == 17)
+                    {
+                        if (getRightKnee() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setRightKnee('d');
+                            }
+                            else
+                            {
+                                setRightKnee('g');
+                            }
+                            control = false;
+                        }
+                    }
+                    else
+                    {
+                        if (getRightShin() == 'g')
+                        {
+                            targets = qwe.Next(18);
+                        }
+                        else
+                        {
+                            targets = qwe.Next(2);
+                            if (targets == 1)
+                            {
+                                setRightShin('d');
+                            }
+                            else
+                            {
+                                setRightShin('g');
+                            }
+                            control = false;
+                        }
+                    }
                 }
             }
+            checkDamage();
         }
     }
 }
